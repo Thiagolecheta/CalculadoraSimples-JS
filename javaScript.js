b0dd492638c90bb2861709751a3f4010676f532e
Thiagolecheta <thiago.lecheta1@gmail.com>
2022-06-10 12:38:52 -0300

    Pequeno projeto de uma calculador simples para exercitar JavaScript

---
+++ javaScript.js
@@ -0,0 +1,31 @@
+function somar(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+
+    let soma = n1 + n2
+    alert("Soma é "+soma)
+}
+
+function subtrair(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+
+    let subtrair = n1 - n2
+    alert("Soma é "+subtrair)
+}
+
+function mult(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+
+    let mult = n1 * n2
+    alert("Soma é "+mult)
+}
+
+function dividir(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+
+    let dividir = n1 / n2
+    alert("Soma é "+dividir)
+}
